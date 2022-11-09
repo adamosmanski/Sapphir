d84039b0aa257c7d643fca8fc099edd3097e8f93
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-09 20:07:03 +0100

    Update NewProjectVM.cs

--- SapphirApp/ViewModels/NewProjectVM.cs
+++ SapphirApp/ViewModels/NewProjectVM.cs
@@ -1,4 +1,5 @@
 ﻿using SapphirApp.Core;
+using Proj = SapphirApp.Data.Models;
 using SapphirApp.Models;
 using System;
 using System.Collections.Generic;
@@ -7,15 +8,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using SapphirApp.Data.Context;
+using SapphirApp.Data.Repository;
 
 namespace SapphirApp.ViewModels
 {
     public class NewProjectVM : ObserveObject
     {
+        ProjectRepository ProjectDTO;
+        SapphirApplicationContext context = new SapphirApplicationContext();
         public NewProjectVM()
         {
+            ProjectDTO = new ProjectRepository(context);
             IsVisible = _isVisible;
             CmdClose = new RelayCommand(Close);
+            CmdAdd = new RelayCommand(Add);
         }
         private Project _project = new Project();
         private bool _isVisible = true;
@@ -37,9 +44,13 @@ namespace SapphirApp.ViewModels
                 OnPropertyChanged(nameof(Project));
             }
         }
-        public ObservableCollection<Project> Projects { get; set; }
-
         public ICommand CmdClose { get; }
+        public ICommand CmdAdd { get; }
+        private void Add(object obj)
+        {
+            ProjectDTO.AddProject(Project.Name, Project.Description, LoggedUser.ID);
+            IsVisible = false;
+        }
         private void Close(object obj)
         {
             IsVisible = false;
