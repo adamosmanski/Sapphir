544b0bfd46643a6925d6b4466cf47e48de28f879
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-09 20:02:16 +0100

    Update Project.cs

--- SapphirApp/Models/Project.cs
+++ SapphirApp/Models/Project.cs
@@ -9,7 +9,15 @@ namespace SapphirApp.Models
     public class Project
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public int? ModUser { get; set; }
+
+        public DateTime? ModDate { get; set; }
     }
 }
