d0df18f98f44eb0b85373d8e79682b763ab50b38
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-09 20:07:01 +0100

    Create ProjectBoardVM.cs

---
+++ SapphirApp/ViewModels/ProjectBoardVM.cs
@@ -0,0 +1,39 @@
+﻿using SapphirApp.Core;
+using SapphirApp.Data.Context;
+using SapphirApp.Data.Repository;
+using SapphirApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace SapphirApp.ViewModels
+{
+    public class ProjectBoardVM : ObserveObject
+    {
+        ProjectRepository ProjectDTO;
+        SapphirApplicationContext context = new SapphirApplicationContext();
+        public ObservableCollection<Project> ListBoxSource { get; set; }
+
+        private Project _selectedBoard = new Project();
+        public Project SelectedBoard
+        {
+            get { return _selectedBoard; }
+            set
+            {
+                _selectedBoard = value;
+                OnPropertyChanged(nameof(SelectedBoard));
+            }
+        }
+        public ProjectBoardVM()
+        {
+            ProjectDTO = new ProjectRepository(context);
+            ListBoxSource = new ObservableCollection<Project>((IEnumerable<Project>)ProjectDTO.GetAll());
+        }
+
+
+    }
+}
