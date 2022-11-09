eee72f4ea38ce9dc2b0c121f79f48eb4ad6ad8ad
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-09 20:06:47 +0100

    Create IProjectRepository.cs

---
+++ SapphireApp.Data/Interface/IProjectRepository.cs
@@ -0,0 +1,15 @@
+﻿using SapphirApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapphirApp.Data.Interface
+{
+    public interface IProjectRepository
+    {
+        void AddProject(string NameProject, string DescriptionProject, int ID);
+        //List<Project> GetAll();
+    }
+}
