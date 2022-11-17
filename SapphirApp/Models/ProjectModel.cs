b9927ae4d2c1df599f7e629dbbdc7abbef0aa71b
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-17 19:33:35 +0100

    Create ProjectModel.cs

---
+++ SapphirApp/Models/ProjectModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapphirApp.Models
+{
+    public class ProjectModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public int? ModUser { get; set; }
+
+        public DateTime? ModDate { get; set; }
+    }
+}
