5452f3e31b0c8b27a3ec9b24163d68a3c87c10b4
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-17 19:33:43 +0100

    Create ButtonVisibilityConverter.cs

---
+++ SapphirApp/Converter/ButtonVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace SapphirApp.Converter
+{
+    public class ButtonVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && (bool)value ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
