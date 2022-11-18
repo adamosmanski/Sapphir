47c546906d2442a8fead28b7a4d2f1a227b244c1
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-18 20:57:40 +0100

    Create TasksProject.cs

---
+++ SapphireApp.Data/Models/TasksProject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SapphirApp.Data.Models;
+
+public partial class TasksProject
+{
+    public int Id { get; set; }
+
+    public int IdProjects { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public string? AssignedUser { get; set; }
+
+    public string? Tag { get; set; }
+
+    public DateTime? ModDate { get; set; }
+
+    public int? ModUser { get; set; }
+}
