87a87d03da444a3a5690529db98245476a37b282
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-19 00:36:30 +0100

    Create TaskRepository.cs

---
+++ SapphireApp.Data/Repository/TaskRepository.cs
@@ -0,0 +1,33 @@
+﻿using SapphirApp.Data.Interface;
+using SapphirApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapphirApp.Data.Repository
+{
+    public class TaskRepository : IKanbanRepository
+    {
+        public void AddTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ArchiveTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TasksProject> GetAllTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
