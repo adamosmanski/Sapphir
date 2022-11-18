31ee53a1e586b1e133a652e00369dc9dfb412121
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-18 20:51:52 +0100

    Create SelectedProject.cs

---
+++ SapphirApp/Models/SelectedProject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapphirApp.Models
+{
+    public static class SelectedProject
+    {
+        public static int? ID { get; set; }
+        public static string? Name { get; set; }        
+    }
+}
