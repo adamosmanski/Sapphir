6b5bcccf51ecb9d56f7ad54b3879722402fda772
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-20 15:55:25 +0100

    update

--- SapphirApp/ViewModels/KanbanBoardVM.cs
+++ SapphirApp/ViewModels/KanbanBoardVM.cs
@@ -19,6 +19,7 @@ namespace SapphirApp.ViewModels
         SapphirApplicationContext context = new SapphirApplicationContext();
         TaskRepository tasksRepository;
         private bool _IsGridVisible = false;
+        private bool _isKanbanEnabled = true;
         public bool IsGridVisible
         {
             get => _IsGridVisible;
@@ -28,21 +29,37 @@ namespace SapphirApp.ViewModels
                 OnPropertyChanged(nameof(IsGridVisible));
             }
         }
+        public bool IsKanbanEnabled
+        {
+            get => _isKanbanEnabled;
+            set
+            {
+                _isKanbanEnabled = value;
+                OnPropertyChanged(nameof(IsKanbanEnabled));
+            }
+        }
         public KanbanBoardVM()
         {
             IsGridVisible = _IsGridVisible;
             tasksRepository = new TaskRepository(context);
             Tasks = DtoTasksToModel.Transform(tasksRepository.GetAllTasks(SelectedProject.ID));
             ShowGridToAddTask = new RelayCommand(ShowGridWithTask);
+            CancelTask = new RelayCommand(CancelAddTask);
         }
+
         public List<TaskProject> Tasks { get; set; }
         public ICommand ShowGridToAddTask { get; }
-        public void ShowGridWithTask(object obj)
+        public ICommand CancelTask { get; }
+        private void ShowGridWithTask(object obj)
         {
            // SelectedProject.Number = tasksRepository.GetLastNumberTask(SelectedProject.ID);
             IsGridVisible = true;
-            
+            IsKanbanEnabled = false;
         }
-
+        private void CancelAddTask(object obj)
+        {
+            IsGridVisible = false;
+            IsKanbanEnabled = true;
+        }
     }
 }
