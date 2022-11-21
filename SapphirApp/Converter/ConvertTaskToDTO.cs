686000b4ffd767e91d8920941f638aa4e33f8d86
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-21 21:04:07 +0100

    Create ConvertTaskToDTO.cs

---
+++ SapphirApp/Converter/ConvertTaskToDTO.cs
@@ -0,0 +1,29 @@
+﻿using SapphirApp.Data.Models;
+using SapphirApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapphirApp.Converter
+{
+    public static class ConvertTaskToDTO
+    {
+        public static TasksProject Transform(NewTask newTask)
+        {
+            TasksProject result = new TasksProject();
+            result.AssignedUser = newTask.AssignedUser;
+            result.IdProjects = newTask.ProjectID;
+            result.Name = newTask.Name;
+            result.ShortNumber = newTask.ShortNumber;
+            result.Tag = newTask.Tag;
+            result.Category = newTask.Category;
+            result.Description = newTask.Description;
+            result.ModDate = newTask.ModDate;
+            result.CreatedAt = newTask.CreatedTime;
+            result.ModUser = newTask.CreatedByID;
+            return result;
+        }
+    }
+}
