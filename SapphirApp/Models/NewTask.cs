a07918bf1980ea7e0e47bc8c18016f12e8071c42
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-21 21:04:05 +0100

    Create NewTask.cs

---
+++ SapphirApp/Models/NewTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapphirApp.Models
+{
+    public class NewTask
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AssignedUser { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public int ProjectID { get; set; }
+        public string ShortNumber { get; set; }
+        public string Category { get; set; }
+        public int CreatedByID { get; set; }
+        public DateTime ModDate { get; set; }
+        public string Tag { get; set; }
+    }
+}
