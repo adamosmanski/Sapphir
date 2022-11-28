26a1e94ccb90a14581b56044e30dad0e62e7afc2
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-11-28 18:35:53 +0100

    1

--- SapphirApp/obj/Debug/net6.0-windows/Views/NotifyPopUp.g.i.cs
+++ SapphirApp/obj/Debug/net6.0-windows/Views/NotifyPopUp.g.i.cs
@@ -54,7 +54,7 @@ namespace SapphirApp.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SapphirApp;V1.0.0.0;component/views/notifypopup.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SapphirApp;component/views/notifypopup.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\NotifyPopUp.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
